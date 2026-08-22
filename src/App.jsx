d32634829e11ebaa273fb6d72adc47f1470bbaf8
Iman <okono3672@gmail.com>
2026-08-22 16:09:37 +0100

    Wire PhoneHub login and signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,54 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
-import PhoneCard from './components/PhoneCard'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import PhoneDetails from './pages/PhoneDetails'
-import Navbar from './components/Navbar'
 import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 import CartPopup from './components/CartPopup'
 import { useState, useEffect } from 'react'
 
-
 function App() {
-  
   const [cart, setCart] = useState([])
   const [search, setSearch] = useState("")
-  const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState("")
   const [cartPopup, setCartPopup] = useState(false)
   const [darkMode, setDarkMode] = useState(false)
-
-//   LOCATION ROUTING
-  const location = useLocation();
+  const location = useLocation()
 
   useEffect(() => {
     if (location.hash === "#categories") {
-        setTimeout(() => {
-            document.getElementById("categories")?.scrollIntoView({
-                behavior: "smooth",
-                block: "start"
-            });
-        }, 100);
+      setTimeout(() => document.getElementById("categories")?.scrollIntoView({ behavior: "smooth", block: "start" }), 100)
     } else {
-        window.scrollTo({
-            top: 0,
-            behavior: "instant"
-        });
+      window.scrollTo({ top: 0, behavior: "instant" })
     }
-}, [location.pathname, location.search, location.hash]);
+  }, [location.pathname, location.search, location.hash])
 
+  const addToCart = (phone) => {
+    const existingItem = cart.find((item) => item.id === phone.id)
+    if (existingItem) setCart(cart.map((item) => item.id === phone.id ? { ...item, quantity: item.quantity + 1 } : item))
+    else setCart([...cart, { ...phone, quantity: 1 }])
+  }
+  const increaseQuantity = (id) => setCart(cart.map((item) => item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
+  const decreaseQuantity = (id) => setCart(cart.map((item) => item.id === id ? { ...item, quantity: item.quantity - 1 } : item).filter((item) => item.quantity > 0))
+  const removeFromCart = (id) => setCart(cart.filter((item) => item.id !== id))
 
-const addToCart = (phone) => {
-    const existingItem = cart.find((item) => item.id === phone.id);
-    if (existingItem) {
-
-        setCart(
-            cart.map((item) =>
-                item.id === phone.id
-                    ? { ...item, quantity: item.quantity + 1 }
-                    : item
-            )
-        );
-
-    } else {
-
-        setCart([
-            ...cart,
-            {
-                ...phone,
-                quantity: 1,
-            },
-        ]);
-
-    }
-
-};
-const increaseQuantity = (id) => {
-
-    setCart(
-        cart.map((item) =>
-            item.id === id
-                ? { ...item, quantity: item.quantity + 1 }
-                : item
-        )
-    );
-
-};
-const decreaseQuantity = (id) => {
-
-    setCart(
-        cart
-            .map((item) =>
-                item.id === id
-                    ? { ...item, quantity: item.quantity - 1 }
-                    : item
-            )
-            .filter((item) => item.quantity > 0)
-    );
-
-};
-const removeFromCart = (id) => {
-
-    setCart(
-        cart.filter((item) => item.id !== id)
-    );
-
-};
-
+  const authPage = location.pathname === "/login" || location.pathname === "/signup"
 
   return (
-
-    <div className= {darkMode ? "darkMode" : "" } >
-
-      <Navbar
-        cart={cart}
-        cartPopup = {cartPopup}
-        setCartPopup = {setCartPopup}
-        increaseQuantity={increaseQuantity}
-        decreaseQuantity={decreaseQuantity}
-        removeFromCart={removeFromCart}
-        search={search}
-        setSearch={setSearch}
-        searchInput={searchInput}
-        setSearchInput={setSearchInput}
-        darkMode= { darkMode }
-        setDarkMode = { setDarkMode }
-      />
-        <Routes>
-          <Route path = "/" element = { <Home addToCart={addToCart} search={search}  /> } />
-          <Route path = "/contact" element = { <Contact /> } />
-          <Route path = "/phones" element = { <PhoneDetails addToCart={addToCart} search={search} /> } />
-        </Routes>
-        { cartPopup && <CartPopup
-              setCartPopup = {setCartPopup}
-              cart={cart}
-              increaseQuantity={increaseQuantity}
-              decreaseQuantity={decreaseQuantity}
-              removeFromCart={removeFromCart}
-              /> }
-        
+    <div className={darkMode ? "darkMode" : ""}>
+      {!authPage && <Navbar cart={cart} cartPopup={cartPopup} setCartPopup={setCartPopup} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} removeFromCart={removeFromCart} search={search} setSearch={setSearch} searchInput={searchInput} setSearchInput={setSearchInput} darkMode={darkMode} setDarkMode={setDarkMode} />}
+      <Routes>
+        <Route path="/" element={<Home addToCart={addToCart} search={search} />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/phones" element={<PhoneDetails addToCart={addToCart} search={search} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {cartPopup && !authPage && <CartPopup setCartPopup={setCartPopup} cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} removeFromCart={removeFromCart} />}
     </div>
-    
   )
 }
 
