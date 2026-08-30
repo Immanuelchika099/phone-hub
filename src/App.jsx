e55ca6457969c5a20cc48428e9a1661263e6536d
Iman <okono3672@gmail.com>
2026-08-30 16:25:56 +0100

    feat: add phone product route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import PhoneDetails from './pages/PhoneDetails'
+import PhoneProduct from './pages/PhoneProduct'
 import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -36,6 +37,7 @@ function App() {
     if (existingItem) setCart(cart.map((item) => item.id === phone.id ? { ...item, quantity: item.quantity + 1 } : item))
     else setCart([...cart, { ...phone, quantity: 1 }])
   }
+
   const increaseQuantity = (id) => setCart(cart.map((item) => item.id === id ? { ...item, quantity: item.quantity + 1 } : item))
   const decreaseQuantity = (id) => setCart(cart.map((item) => item.id === id ? { ...item, quantity: item.quantity - 1 } : item).filter((item) => item.quantity > 0))
   const removeFromCart = (id) => setCart(cart.filter((item) => item.id !== id))
@@ -49,6 +51,7 @@ function App() {
         <Route path="/" element={<Home addToCart={addToCart} search={search} />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/phones" element={<PhoneDetails addToCart={addToCart} search={search} />} />
+        <Route path="/phones/:id" element={<PhoneProduct addToCart={addToCart} />} />
         <Route path="/login" element={<Login darkMode={darkMode} setDarkMode={setDarkMode} />} />
         <Route path="/signup" element={<Signup darkMode={darkMode} setDarkMode={setDarkMode} />} />
         <Route path="*" element={<NotFound />} />
